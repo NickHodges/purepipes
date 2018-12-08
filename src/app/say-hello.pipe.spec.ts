9120e6387f08187366d30f97206d6fcfe6a424af
Nick Hodges <nickhodges@gmail.com>
2018-12-08 16:58:08 -0500

    Remove testing stuff

--- src/app/say-hello.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SayHelloPipe } from './demo-pipes.pipe';
-
-describe('SayHelloPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SayHelloPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
